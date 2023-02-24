3ad16e266468b53f84f9b6d8f05e1ebf6cfd2cf6
unknown <nehalamohamed9@gmail.com>
2023-02-24 21:23:48 +0530

    fir pro

---
+++ travelloapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import place
+# Create your views here.
+def fun(request):
+    obj=place.objects.all()
+
+
+
+    return render(request,'index.html',{'results':obj})
